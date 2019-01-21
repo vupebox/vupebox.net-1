14f497afb146159c711a36d4337b8b6483d96626
Alejandro.jimenez <alejandro.jimenez@serviciosju.com>
2019-01-21 08:52:48 -0700

    Mi primer commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+// settings
+app.set('port', 8000);
+app.set('views',path.join(__dirname, 'views'));
+app.engine('html',require('ejs').renderFile);
+app.set('view engine','ejs');
+
+//middlewares
+
+//routes
+app.use(require('./routes/index'));
+ 
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+//listening the server
+app.listen(app.get('port'), ()=>{
+    console.log('servidor iniciado.', app.get('port'));
+
+});
---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req, res)=>{
+    res.render('index.html', {title: 'First Web site'});
+});
+router.get('/contact',(req, res)=>{
+    res.render('contact.html', {title:'Contact page'});
+});
+module.exports= router;

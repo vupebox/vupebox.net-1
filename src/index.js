810c2578fd4d7b3416540b964595059b6b5031cb
vupebox <45156746+vupebox@users.noreply.github.com>
2019-02-10 17:22:37 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 // settings
-app.set('port', 8000);
+app.set('port', 3000);
 app.set('views',path.join(__dirname, 'views'));
 app.engine('html',require('ejs').renderFile);
 app.set('view engine','ejs');
